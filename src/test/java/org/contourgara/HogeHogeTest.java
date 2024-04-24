41158600970239dfc6caf31f487e26befdab679e
contour-gara <65527071+contour-gara@users.noreply.github.com>
2024-04-24 16:16:33 +0900

    パラメータ化テストができるように修正

--- src/test/java/org/contourgara/HogeHogeTest.java
+++ src/test/java/org/contourgara/HogeHogeTest.java
@@ -1,8 +1,8 @@
 package org.contourgara;
 
-import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
 
 class HogeHogeTest {
   @Test
